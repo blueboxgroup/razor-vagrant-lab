b2de1004636c795b204d046db681494d47c4d8b9
Fletcher Nichol <fnichol@nichol.ca>
2012-12-31 09:19:46 -0700

    Trim back internal router cookbook.

--- cookbooks-internal/router/metadata.rb
+++ cookbooks-internal/router/metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Fletcher Nichol"
 maintainer_email  "fnichol@nichol.ca"
 license           "Apache 2.0"
 description       "Quick and dirty NAT router with iptables"
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+long_description  "Quick and dirty NAT router with iptables"
 version           "0.1.0"
 
 supports "ubuntu"
